3bab26703248fadedbc475e133c1f99a36b5e73d
jaro2gw <p.jaronsky@gmail.com>
2023-12-03 21:27:13 +0100

    Add Advent of Code 2023 module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,6 @@ gradleEnterprise {
 
 rootProject.name = "advent-of-code"
 include("advent-of-code-base")
-include("advent-of-code-2022")
 include("advent-of-code-test")
+include("advent-of-code-2022")
+include("advent-of-code-2023")
