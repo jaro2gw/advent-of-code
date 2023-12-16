61d1eda6d2bc329634337100e9cc198a44abfb7b
Piotr Jaroński <p.jaronsky@gmail.com>
2023-12-16 21:34:34 +0100

    Change gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,3 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-    id("com.gradle.enterprise") version "3.15.1"
-}
-
-gradleEnterprise {
-    if (System.getenv("CI") == "true") {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
-    }
-}
-
 rootProject.name = "advent-of-code"
 include("advent-of-code-base")
 include("advent-of-code-test")
